fe8b3a29fafda0d832777e36006d42a8e9062d4a
SantiCalabro <santi.calabro93@gmail.com>
2024-12-03 09:46:57 -0300

    vite project and tailwind implementation

---
+++ portfolio/src/components/NavBar.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export default function NavBar() {
+  return (
+    <div>
+      <button className="btn-primary poppins">Primary Button</button>
+      <button className="btn-secondary roboto">Secondary Button</button>
+    </div>
+  )
+}
