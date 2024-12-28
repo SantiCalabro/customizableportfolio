6bb95941d63c9fc13acdf9ba5d0fd597fbc8f91e
TestUser <usertest@MacBook-Pro-i5.local>
2024-12-28 12:39:22 -0300

    contact form

--- portfolio/src/components/Dropdown.jsx
+++ portfolio/src/components/Dropdown.jsx
@@ -4,7 +4,7 @@ import JsIcon from "../assets/JavaScript.svg";
 
 const Dropdown = () => {
   const [isOpen, setIsOpen] = useState({
-    dev: false,
+    dev: true,
     design: false,
   });
 
@@ -37,16 +37,14 @@ const Dropdown = () => {
     <div className="relative inline-block text-left" ref={dropdownRef}>
       <button
         onClick={() => toggleDropdown("dev")}
-        className="poppins inline-flex w-full border-b-mandarine px-10 pt-6 pb-4 text-2xl mandarine font-bold bg-primary-black focus:outline-none"
-      >
+        className="poppins inline-flex w-full border-b-mandarine px-10 pt-6 pb-4 text-2xl mandarine font-bold bg-primary-black focus:outline-none">
         Development
         <svg
           className="w-5 h-5 ml-2 -mr-1"
           xmlns="http://www.w3.org/2000/svg"
           viewBox="0 0 20 20"
           fill="currentColor"
-          aria-hidden="true"
-        >
+          aria-hidden="true">
           <path
             fillRule="evenodd"
             d="M5.293 7.707a1 1 0 011.414 0L10 11.586l3.293-3.879a1 1 0 111.414 1.414l-4 4.5a1 1 0 01-1.414 0l-4-4.5a1 1 0 010-1.414z"
@@ -56,28 +54,33 @@ const Dropdown = () => {
       </button>
       {isOpen.dev && (
         <div
-          className="absolute right-0 py-10 w-full origin-top-right bg-dark shadow-lg ring-1 ring-black ring-opacity-5 transition-all duration-300 ease-in-out max-h-[500px] opacity-100 overflow-hidden"
-          style={{ maxHeight: isOpen.dev ? "500px" : "0", opacity: isOpen.dev ? 1 : 0 }}
-        >
+          className=" right-0 py-10 w-full origin-top-right bg-dark shadow-lg ring-1 ring-black ring-opacity-5 transition-all duration-300 ease-in-out max-h-[500px] opacity-100 overflow-hidden"
+          style={{
+            maxHeight: isOpen.dev ? "500px" : "0",
+            opacity: isOpen.dev ? 1 : 0,
+          }}>
           <div className="grid grid-cols-1 sm:grid-cols-2 sm:px-4 md:grid-cols-3 md:px-8 lg:grid-cols-4 lg:px-10 xl:grid-cols-5 2xl:grid-cols-7 2xl:px-40 gap-4 place-items-center">
             {Array.from({ length: 10 }).map((_, index) => (
-              <Chip key={index} icon={JsIcon} alt="JavaScript" tech="JavaScript" />
+              <Chip
+                key={index}
+                icon={JsIcon}
+                alt="JavaScript"
+                tech="JavaScript"
+              />
             ))}
           </div>
         </div>
       )}
       <button
         onClick={() => toggleDropdown("design")}
-        className="poppins inline-flex w-full border-b-mandarine px-10 pt-6 pb-4 text-2xl mandarine font-bold bg-primary-black focus:outline-none"
-      >
+        className="poppins inline-flex w-full border-b-mandarine px-10 pt-6 pb-4 text-2xl mandarine font-bold bg-primary-black focus:outline-none">
         Design
         <svg
           className="w-5 h-5 ml-2 -mr-1"
           xmlns="http://www.w3.org/2000/svg"
           viewBox="0 0 20 20"
           fill="currentColor"
-          aria-hidden="true"
-        >
+          aria-hidden="true">
           <path
             fillRule="evenodd"
             d="M5.293 7.707a1 1 0 011.414 0L10 11.586l3.293-3.879a1 1 0 111.414 1.414l-4 4.5a1 1 0 01-1.414 0l-4-4.5a1 1 0 010-1.414z"
@@ -87,12 +90,19 @@ const Dropdown = () => {
       </button>
       {isOpen.design && (
         <div
-          className="absolute right-0 py-10 w-full origin-top-right bg-dark shadow-lg ring-1 ring-black ring-opacity-5 transition-all duration-300 ease-in-out max-h-[500px] opacity-100 overflow-hidden"
-          style={{ maxHeight: isOpen.design ? "500px" : "0", opacity: isOpen.design ? 1 : 0 }}
-        >
+          className=" right-0 py-10 w-full origin-top-right bg-dark shadow-lg ring-1 ring-black ring-opacity-5 transition-all duration-300 ease-in-out max-h-[500px] opacity-100 overflow-hidden"
+          style={{
+            maxHeight: isOpen.design ? "500px" : "0",
+            opacity: isOpen.design ? 1 : 0,
+          }}>
           <div className="grid grid-cols-1 sm:grid-cols-2 sm:px-4 md:grid-cols-3 md:px-8 lg:grid-cols-4 lg:px-10 xl:grid-cols-5 2xl:grid-cols-7 2xl:px-40 gap-4 place-items-center">
             {Array.from({ length: 10 }).map((_, index) => (
-              <Chip key={index} icon={JsIcon} alt="JavaScript" tech="JavaScript" />
+              <Chip
+                key={index}
+                icon={JsIcon}
+                alt="JavaScript"
+                tech="JavaScript"
+              />
             ))}
           </div>
         </div>
--- portfolio/src/components/Form.jsx
+++ portfolio/src/components/Form.jsx
@@ -1,7 +1,8 @@
-import { useState } from "react";
+import React, { useState } from "react";
 import contactIcon from "../assets/ContactBlack.svg";
+
 export default function Contact() {
-  const [input, setInput] = useState({
+  const [formData, setFormData] = useState({
     fullName: "",
     companyName: "",
     email: "",
@@ -10,47 +11,40 @@ export default function Contact() {
 
   const [errors, setErrors] = useState({});
   const [touched, setTouched] = useState({});
-  const [disabled, setDisabled] = useState(true);
 
   const validate = () => {
     const newErrors = {};
-    const emailRegex = /\S+@\S+\.\S+/;
-    if (!input.fullName) newErrors.fullName = "Full Name is required.";
-    if (!input.companyName) newErrors.companyName = "Company Name is required.";
-    if (!input.email) {
+    if (!formData.fullName) newErrors.fullName = "Full Name is required.";
+    if (!formData.companyName)
+      newErrors.companyName = "Company Name is required.";
+    if (!formData.email) {
       newErrors.email = "Email is required.";
-    } else if (!emailRegex.test(input.email)) {
-      newErrors.email = "Email must be valid.";
-    } else if (!input.message) {
-      newErrors.message = "Please write your message.";
-    }
-    if (
-      !newErrors.fullName &&
-      !newErrors.companyName &&
-      !newErrors.email &&
-      !newErrors.message &&
-      input.message.length > 0
-    ) {
-      setDisabled(false);
-    } else {
-      setDisabled(true);
+    } else if (!/\S+@\S+\.\S+/.test(formData.email)) {
+      newErrors.email = "Email is invalid.";
     }
     return newErrors;
   };
 
   const handleChange = (e) => {
     const { name, value } = e.target;
-    setInput((prev) => ({ ...prev, [name]: value }));
+    setFormData((prev) => ({ ...prev, [name]: value }));
     if (touched[name]) {
-      setErrors((prev) => ({ ...prev, [name]: validate()[name] }));
+      setErrors((prev) => ({
+        ...prev,
+        [name]: validate()[name],
+      }));
     }
   };
 
   const handleBlur = (e) => {
     const { name } = e.target;
     setTouched((prev) => ({ ...prev, [name]: true }));
-    setErrors((prev) => ({ ...prev, [name]: validate()[name] }));
+    setErrors((prev) => ({
+      ...prev,
+      [name]: validate()[name],
+    }));
   };
+
   const handleSubmit = (e) => {
     e.preventDefault();
     const newErrors = validate();
@@ -61,11 +55,12 @@ export default function Contact() {
       email: true,
       message: true,
     });
-    setDisabled(true);
     if (Object.keys(newErrors).length === 0) {
-      alert("Form submitted");
+      alert("Form submitted successfully!");
+      // Aquí iría la lógica para enviar el formulario, como una API call.
     }
   };
+
   return (
     <div id="contact" className="flex justify-center items-center my-20">
       <div className="rounded-xl bg-white w-4/5 flex flex-col justify-center items-center p-10 px-20">
@@ -73,7 +68,7 @@ export default function Contact() {
         <h3 className="font-bold poppins dark text-2xl text-center mt-2">
           Let's Connect!
         </h3>
-        <p className="poppins text-sm dark text-center mt-2 ">
+        <p className="poppins text-sm dark text-center mt-2">
           If you have any questions, a project in mind or a comment, you can
           write to me by completing the following form.
         </p>
@@ -98,33 +93,22 @@ export default function Contact() {
                 type: "email",
                 placeholder: "Email",
               },
-              {
-                label: "Message*",
-                name: "message",
-                type: "textarea",
-                placeholder: "Your message",
-              },
             ].map((field) => (
-              <div key={field.name} className="flex flex-col w-full my-2">
+              <div key={field.name} className="flex flex-col w-full mb-4">
                 <label className="poppins mb-1 text-sm">{field.label}</label>
-                {field.name === "message" ? (
-                  <textarea
-                    placeholder={field.placeholder}
-                    className="border border-gray rounded-md poppins p-2 text-sm h-36 focus:outline-none"
-                    name={field.name}
-                    onChange={handleChange}
-                    onBlur={handleBlur}
-                  />
-                ) : (
-                  <input
-                    onChange={handleChange}
-                    name={field.name}
-                    onBlur={handleBlur}
-                    type={field.type}
-                    placeholder={field.placeholder}
-                    className="border border-gray rounded-md poppins p-2 text-sm focus:outline-none"
-                  />
-                )}
+                <input
+                  type={field.type}
+                  name={field.name}
+                  placeholder={field.placeholder}
+                  value={formData[field.name]}
+                  onChange={handleChange}
+                  onBlur={handleBlur}
+                  className={`border rounded-md poppins p-2 text-sm focus:outline-none ${
+                    errors[field.name] && touched[field.name]
+                      ? "border-red-500"
+                      : "border-gray-300"
+                  }`}
+                />
                 {errors[field.name] && touched[field.name] && (
                   <p className="poppins text-xs mt-1 text-red-500">
                     {errors[field.name]}
@@ -132,11 +116,21 @@ export default function Contact() {
                 )}
               </div>
             ))}
+            <div className="flex flex-col w-full mb-4">
+              <label className="poppins mb-1 text-sm">Message</label>
+              <textarea
+                name="message"
+                placeholder="Message"
+                value={formData.message}
+                onChange={handleChange}
+                onBlur={handleBlur}
+                className="border border-gray-300 rounded-md poppins p-2 text-sm h-36 focus:outline-none"
+              />
+            </div>
             <button
               type="submit"
-              className={`poppins font-bold font-sm ${
-                disabled ? "disabled bg-secondary-gray" : "bg-primary-black"
-              }  px-20 py-1 rounded-full white mt-3`}>
+              className="poppins font-bold font-sm bg-secondary-gray px-20 py-1 rounded-full white mt-3"
+            >
               Submit
             </button>
           </form>
